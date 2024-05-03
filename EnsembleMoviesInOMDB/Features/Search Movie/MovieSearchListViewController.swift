a0e82ffbbbe715992c78f711cbe95ea66e8d5155
Lucas C Barros <Lucas_cb@icloud.com>
2024-05-02 23:32:39 -0700

    Added app logo and splashScreen

--- EnsembleMoviesInOMDB/Features/Search Movie/MovieSearchListViewController.swift
+++ EnsembleMoviesInOMDB/Features/Search Movie/MovieSearchListViewController.swift
@@ -197,20 +197,3 @@ extension MovieSearchListViewController {
                                                            action: #selector(tapCustomCellFeatureButton))
     }
 }
-
-extension UIViewController {
-    func popAlert(title: String, message: String) {
-        // create the alert
-        let alert = UIAlertController(title: title,
-                                      message: message,
-                                      preferredStyle: UIAlertController.Style.alert)
-        
-        // add an action (button)
-        alert.addAction(UIAlertAction(title: "OK",
-                                      style: UIAlertAction.Style.default,
-                                      handler: nil))
-        
-        // show the alert
-        self.present(alert, animated: true, completion: nil)
-    }
-}
